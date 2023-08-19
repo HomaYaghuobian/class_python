4c2871cddcc6ef3adadf0ae2e4fb55489f18235f
HomaYaghoubian <140716080+HomaYaghuobian@users.noreply.github.com>
2023-08-19 16:54:24 +0330

    Add files via upload

--- J8/exrercise/prime3.py
+++ J8/exrercise/prime3.py
@@ -1,11 +1,11 @@
-# کد هنوز اماده نیست
 a = int(input('a: '))
 b = int(input('b: '))
-c = 1
-r = 0
+
 prime = []
-if b > a: #*****************
+if b > a:
     for i in range(a+1,b):
+        c = 1
+        r = 0
         while (i != 1) or (i != 0):
             if i % c == 0 :
                 r += 1
@@ -14,29 +14,33 @@ if b > a: #*****************
                 continue
             elif c > i:
                 break
-            if r > 2 :
+        if r > 2 or r < 2:
+            continue
+        elif r == 2:
+            prime.append(i)
+
+elif a > b:
+    for i in range(b+1,a):
+        c = 1
+        r = 0
+        while (i != 1) or (i != 0):
+            if i % c == 0 :
+                r += 1
+            if c <= i:
+                c += 1
                 continue
-            if r < 2:
-                continue
-            elif r == 2:
-                prime.append(i)
+            elif c > i:
+                break
+        if r > 2 :
+            continue
+        if r < 2:
+            continue
+        elif r == 2:
+            prime.append(i)
+            continue
 
-# elif a > b:
-#     for i in range(b+1,a):
-#         while True:
-#             if i % c == 0 :
-#                 r += 1
-#             if c <= i:
-#                 c += 1
-#                 continue
-#             elif c > i:
-#                 break
-#         if r > 2 :
-#             continue
-#         elif r == 2:
-#             prime.append(i)
+else:
+    print('None')
 
-# else:
-#     print('None')
-
-print(prime)
+if len(prime) != 0:
+    print(prime)
