6bb7ade007080c30b00e089d242f9c7dcad5e677
HomaYaghoubian <140716080+HomaYaghuobian@users.noreply.github.com>
2023-08-25 12:00:12 +0330

    Add files via upload

---
+++ J13/exercise/class4.py
@@ -0,0 +1,105 @@
+# کلاس Person
+class Person:
+    # properties
+    name = None
+    last_name = None
+    nationality = None
+    nationality_id = None
+    heir_color = None
+    interests = None
+    habits = None
+    # methods
+    def eat():
+        pass
+    def sleep():
+        pass
+    def working():
+        pass
+    def walking():
+        pass
+    def to_laugh():
+        pass
+    def to_live():
+        pass
+    def talking():
+        pass
+
+# کلاس Student
+class Student:
+    # properties
+    name = None
+    last_name = None
+    study = None
+    nationality_id = None
+    mean = None
+    # methods
+    def to_study():
+        pass
+    def eat():
+        pass
+    def sleep():
+        pass
+    def to_live():
+        pass
+    def talking():
+        pass
+  
+# کلاس Car
+class Car:
+    # properties
+    model = None
+    component = None
+    color = None
+    plate = None
+    made_year = None
+    # methods
+    def to_move():
+        pass
+    def to_blow_the_horn():
+        pass
+    def to_put_up():
+        pass
+    def to_become_ruined():
+        pass
+    def to_go_fast():
+        pass
+
+# کلاس Animal
+class Animal:
+    # properties
+    kind = None
+    color = None
+    lifetime = None
+    place_live = None
+    its_voice = None
+    # methods
+    def eat():
+        pass
+    def sleep():
+        pass
+    def to_snore():
+        pass
+    def hunt():
+        pass
+    def to_be_hundet():
+        pass
+
+# کلاس Fruit 
+class Fruit:
+    # properties
+    colors = None
+    flavor = None
+    shape = None
+    price = None
+    its_country = None
+    # methods
+    def fret():
+        pass
+    def grow():
+        pass
+    def to_mould():
+        pass
+    def to_be_crushed_or_trod():
+        pass
+    def to_be_bought():
+        pass
